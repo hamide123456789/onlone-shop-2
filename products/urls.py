061d93ce1db7d967d1a77b6876d08463a9e1a3e3
hamide ghafari <hamideh.ghafari2018@gmail.com>
2024-09-16 14:11:17 +0330

    add static file

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView
+from .import views
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='product_list'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
