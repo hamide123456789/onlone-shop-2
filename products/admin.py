0372d7f099d67c2d1874ef866cb4429dbfb37741
hamide ghafari <hamideh.ghafari2018@gmail.com>
2024-09-22 14:54:50 +0330

    add translation to products app

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Product
+from .models import Product, Comment
+
+
+class CommentsInLine(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active', ]
+    extra = 1
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active']
+
+    inlines = [
+        CommentsInLine,
+    ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'body', 'stars', 'active', ]
--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .import views
+from .views import ProductListView, ProductDetailView, CommentCreateView
+from .views import test_translation
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product_list'),
-    path('<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('', ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('comment/<int:product_id>/', CommentCreateView.as_view(), name='comment_create'),
+    path('hello/', test_translation, ),
 ]
