b9360d0b2197f8f7b0d3cdd5507b71a6c8d9b2bf
hamide ghafari <hamideh.ghafari2018@gmail.com>
2024-09-22 15:23:42 +0330

    create Product model

--- config/settings.py
+++ config/settings.py
@@ -136,13 +136,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
